8bc002e7f15f2603491672ee9e2ea4c1b268f995
Rodrigo Pina <pinarodrigo@Pina-iMac.local>
2017-07-27 19:46:35 +0200

    added function to geneate SAS Token

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var azure = require('azure');
+var CryptoJS = require("crypto-js");
 var hubName = 'msazuremobilenativescriptnotificationhub';
 var connectionString = 'Endpoint=sb://msazuremobilenativescript.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=68N0vDpCDjEjERffOBG/U06ju094N7yXmENYE0N8n0Q=';
-var notificationHubService = azure.createNotificationHubService(hubName, connectionString, function(err) {
+var notificationHubService = azure.createNotificationHubService(hubName, connectionString, function (err) {
     if (!err) {
         //Connection to Notification hub was successfull
     } else {
@@ -9,20 +10,33 @@ var notificationHubService = azure.createNotificationHubService(hubName, connect
     }
 });
 
-// template notification
-/*notificationHubService.send(
-    null, {
-        message: 'This is my template notification',
-        goesTo: 'all registrations irrespective of the platform'
-    },
-    function(error) {
-        if (!error) {
-            console.log('message sent successfully');
-        }
-    });*/
+var getSelfSignedToken = function (targetUri, sharedKey, ruleId,
+    expiresInMins) {
+    targetUri = encodeURIComponent(targetUri.toLowerCase()).toLowerCase();
 
+    // Set expiration in seconds
+    var expireOnDate = new Date();
+    expireOnDate.setMinutes(expireOnDate.getMinutes() + expiresInMins);
+    var expires = Date.UTC(expireOnDate.getUTCFullYear(), expireOnDate
+        .getUTCMonth(), expireOnDate.getUTCDate(), expireOnDate
+            .getUTCHours(), expireOnDate.getUTCMinutes(), expireOnDate
+                .getUTCSeconds()) / 1000;
+    var tosign = targetUri + '\n' + expires;
+
+    // using CryptoJS
+    var signature = CryptoJS.HmacSHA256(tosign, sharedKey);
+    var base64signature = signature.toString(CryptoJS.enc.Base64);
+    var base64UriEncoded = encodeURIComponent(base64signature);
+
+    // construct autorization string
+    var token = "SharedAccessSignature sr=" + targetUri + "&sig="
+        + base64UriEncoded + "&se=" + expires + "&skn=" + ruleId;
+    // console.log("signature:" + token);
+    return token;
+};
 
 // get list of regoistrations
-notificationHubService.listRegistrations(function(err, registrations) {
-    console.log(registrations);
+notificationHubService.listRegistrations(function (err, registrations) {
+    //console.log(registrations);
+    console.log(getSelfSignedToken('https://msazuremobilenativescript.servicebus.windows.net/msazuremobilenativescriptnotificationhub','68N0vDpCDjEjERffOBG/U06ju094N7yXmENYE0N8n0Q=','DefaultFullSharedAccessSignature',3600));
 });
